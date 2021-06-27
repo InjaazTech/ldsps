f7c3152a55f18da316570773473ca37b942eb950
Zaher Hazeem <zhazeem@gmail.com>
2021-06-27 15:12:19 +0300

    bug fixes

--- db/migrate/20210525121251_add_extra_fields.rb
+++ db/migrate/20210525121251_add_extra_fields.rb
@@ -1,8 +1,8 @@
 class AddExtraFields < ActiveRecord::Migration[6.1]
   def change
     add_column :posts, :custom_date, :date
-    add_column :wpages, :ar_subtitle, :string, limit: 255
-    add_column :wpages, :en_subtitle, :string, limit: 255
-    add_column :wpages, :tr_subtitle, :string, limit: 255
+    add_column :wpages, :ar_subtitle, :string, limit: 1000
+    add_column :wpages, :en_subtitle, :string, limit: 1000
+    add_column :wpages, :tr_subtitle, :string, limit: 1000
   end
 end

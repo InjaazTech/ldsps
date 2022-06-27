582de6b2b162667e8b0a6d05724b620496548a60
Zaher Hazeem <zhazeem@gmail.com>
2022-06-27 13:06:41 +0300

    fix cdn host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-host = '161.97.68.94'
+host = '52.255.183.18'
 user = 'deployer'
 
 roles = "#{user}@#{host}"
--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     region: ENV['AWS_REGION'],
     endpoint: 'https://ams3.digitaloceanspaces.com'
   }
-  config.asset_host = 'https://www.lalecdn.com'
+  config.asset_host = ENV['CDN_HOST']
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
